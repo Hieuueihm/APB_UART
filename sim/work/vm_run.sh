786287043e151e2771adf7cb1172b5a08295058e
Hieu <gbmhieu123@gmail.com>
2025-05-09 12:06:24 +0700

    add wave simu

--- sim/work/vm_run.sh
+++ sim/work/vm_run.sh
@@ -2,4 +2,4 @@ reset
 
 rm -rf xcelium.d INCA* *.log *.key *.simvis* waves* *.shm
 
-xrun -sv -64BIT -debug_opts verisium_pp -log xrun.log -f file_list.f +tcl+run_batch.tcl
+xrun -sv -64BIT -debug_opts verisium_pp -log xrun.log -f file_list.f -input xlm_waves.tcl +tcl+run_batch.tcl
